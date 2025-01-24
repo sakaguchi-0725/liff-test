2fbd9558024b6f6a81fddc238029f4094bd7a6cc
Kazuma Sakaguchi <sakaguchi@SRC-DPC-MAC-005.local>
2025-01-24 14:49:37 +0900

    modify

--- src/pages/home/ui/home-page.vue
+++ src/pages/home/ui/home-page.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
 import liff from '@line/liff';
+import { ref } from 'vue';
 import { RouterLink } from 'vue-router'
 
-const id = liff.getIDToken()
+const id = ref<string | null>(liff.getIDToken())
 
-console.log(id)
+
 </script>
 
 <template>
   <div class="content">
     <h1 class="page-header">Home</h1>
     <p class="text">
-      これはホーム画面です
+      これはホーム画面です<br />
+      LINE ID {{ id }}
     </p>
     <RouterLink :to="{ name: 'about' }" class="btn btn-primary">
       次へ
