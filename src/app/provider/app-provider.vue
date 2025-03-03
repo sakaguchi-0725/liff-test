2fc3bcf46fe7cc506463b17d90418dec851a1d4d
Kazuma Sakaguchi <sakaguchi@SRC-DPC-MAC-005.local>
2025-03-03 14:59:34 +0900

    modify

--- src/app/provider/app-provider.vue
+++ src/app/provider/app-provider.vue
@@ -1,28 +1,15 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute()
 
-const decodedLiffState = computed(() => {
-  const liffState = route.query['liff.state']
-  alert(liffState)
-  return liffState ? decodeURIComponent(liffState as string) : ''
+
+onMounted(() => {
+  console.log(route.query)
 })
-
-const clinicVal = computed(() => {
-  if (!decodedLiffState.value) return null
-
-  const urlParts = decodedLiffState.value.split('?')
-  if (urlParts.length < 2) return null
-
-  const params = new URLSearchParams(urlParts[1])
-  return params.get('clinic')
-})
-
 </script>
 
 <template>
-  <p>{{ clinicVal }}</p>
   <RouterView />
 </template>
