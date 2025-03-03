298a1658a411a6700c8b444f05d8ec38a70e4b3c
Kazuma Sakaguchi <sakaguchi@SRC-DPC-MAC-005.local>
2025-03-03 15:38:12 +0900

    modify

--- src/app/provider/app-provider.vue
+++ src/app/provider/app-provider.vue
@@ -1,11 +1,10 @@
 <script lang="ts" setup>
-import liff from '@line/liff';
 import { useRoute } from 'vue-router';
 
-liff.ready.then(() => {
-  const route = useRoute()
-  alert(JSON.stringify(route.query))
-})
+
+const route = useRoute()
+alert(JSON.stringify(route))
+alert(window.location.href)
 </script>
 
 <template>
