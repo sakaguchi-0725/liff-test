0c398705fa4d57efbfbcade358a0849e4d70b041
Kazuma Sakaguchi <sakaguchi@SRC-DPC-MAC-005.local>
2025-03-03 16:56:57 +0900

    modify

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { App, createAppRouter } from './app'
 import liff from '@line/liff'
 import { constants } from './shared/lib'
 
-await liff.init({ liffId: constants.LIFF_ID })
+const main = async () => {
+  await liff.init({ liffId: constants.LIFF_ID })
 
-const app = createApp(App)
-const router = createAppRouter("web")
+  const app = createApp(App)
+  const router = createAppRouter("web")
 
-app.use(router)
-app.mount("#app")
+  app.use(router)
+  app.mount("#app")
+}
+
+main()
