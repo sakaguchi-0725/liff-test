bf8b1d0ae9fb4efe40808689d24022d5a2fa196a
Kazuma Sakaguchi <sakaguchi@SRC-DPC-MAC-005.local>
2025-03-03 15:56:30 +0900

    modify

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,22 +1,6 @@
 <script setup lang="ts">
-import '@/app/assets/css/index.css'
-import { RouterView } from 'vue-router'
-import { constants } from '@/shared/lib'
-import liff from "@line/liff"
-import { onMounted } from 'vue'
-import AppProvider from './provider/app-provider.vue'
-
-onMounted(async () => {
-  await liff.init({ liffId: constants.LIFF_ID })
-    .then(() => {
-      alert('LIFF initialized successfully')
-      const token = liff.getAccessToken()
-      alert(token ?? 'getAccessToken failed')
-    })
-    .catch(() => {
-      alert('LIFF initialization failed')
-    })
-})
+import AppProvider from './provider/app-provider.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import { App, createAppRouter } from './app'
+import liff from '@line/liff'
+import { constants } from './shared/lib'
+
+liff.init({ liffId: constants.LIFF_ID })
 
 const app = createApp(App)
 const router = createAppRouter("web")
