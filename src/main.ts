4bda439700e0437db1de4bbd517d27edf2dcffce
Kazuma Sakaguchi <sakaguchi@SRC-DPC-MAC-005.local>
2025-04-07 12:16:44 +0900

    modify

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { constants } from './shared/lib'
 
 const main = async () => {
   await liff.init({ liffId: constants.LIFF_ID })
+  
+  const idToken = liff.getIDToken()
+  const accessToken = liff.getAccessToken()
+  alert(`IDToken: ${idToken}\nAccessToken: ${accessToken}`)
 
   const app = createApp(App)
   const router = createAppRouter("web")
